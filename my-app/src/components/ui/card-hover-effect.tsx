89bd8d03a76e9c8c1af00574e9875a5f6bd9ab2d
Muhammad Mannan <usmanmannan8@gmail.com>
2024-05-01 21:13:39 -0700

    ✨ Add: Updates to article titles and descriptions
    - Updated titles and descriptions for various articles to provide more detailed content
    - Links to articles were also updated to reflect the changes made

--- my-app/src/components/ui/card-hover-effect.tsx
+++ my-app/src/components/ui/card-hover-effect.tsx
@@ -69,7 +69,7 @@ export const Card = ({
   return (
     <div
       className={cn(
-        "rounded-2xl h-full w-full p-4 overflow-hidden border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
+        "rounded-2xl h-full w-full p-4 overflow-hidden border dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
         className,
       )}
       style={{
@@ -91,7 +91,12 @@ export const CardTitle = ({
   children: React.ReactNode;
 }) => {
   return (
-    <h4 className={cn("text-zinc-100 font-bold tracking-wide mt-4", className)}>
+    <h4
+      className={cn(
+        "text-[#0A0A0A] font-bold tracking-wide mt-4 dark:text-white",
+        className,
+      )}
+    >
       {children}
     </h4>
   );
@@ -107,7 +112,7 @@ export const CardDescription = ({
   return (
     <p
       className={cn(
-        "mt-8 text-zinc-400 tracking-wide leading-relaxed text-sm",
+        "mt-8 text-zinc-500 tracking-wide leading-relaxed text-sm",
         className,
       )}
     >
