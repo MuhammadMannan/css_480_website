3f00003a55774d9b4d002982a1ff6447dae5253e
Muhammad Mannan <usmanmannan8@gmail.com>
2024-05-17 23:13:07 -0700

    ℹ️ Add images and description to D&D topic page

--- my-app/src/app/interestingtopic/page.tsx
+++ my-app/src/app/interestingtopic/page.tsx
@@ -29,24 +29,46 @@ const carouselData: CarouselData[] = [
     text: "In the vast realm of Dungeons & Dragons, your character is a reflection of your choices, values, and aspirations. Every detail, from your race and class to your background and alignment, shapes the hero you'll become. With an intricate system of abilities and skills, you can craft a truly unique persona, ready to embark on epic adventures.",
   },
   {
-    image: "https://avatars.githubusercontent.com/u/25876150?v=4",
+    image:
+      "https://github.com/MuhammadMannan/css_480_website/blob/main/my-app/src/assets/image2.png?raw=true",
     heading: "Unleash Your Imagination",
     text: "The worlds of D&D are bound only by the limits of your imagination. Whether you seek to traverse the sprawling cities and untamed wilds of classic fantasy realms or venture into uncharted territories where the impossible becomes reality, the game's rich lore and boundless creativity offer an endless tapestry of possibilities to explore.",
   },
   {
-    image: "https://avatars.githubusercontent.com/u/25876150?v=4",
+    image:
+      "https://github.com/MuhammadMannan/css_480_website/blob/main/my-app/src/assets/image3.png?raw=true",
     heading: "Forge Unbreakable Bonds",
     text: "At its core, D&D is a shared storytelling experience that fosters camaraderie and lasting friendships. As you and your fellow adventurers face perilous challenges, overcome daunting obstacles, and unravel ancient mysteries together, an unbreakable bond is forged – a testament to the power of cooperation, trust, and collective imagination.",
   },
   {
-    image: "https://avatars.githubusercontent.com/u/25876150?v=4",
+    image:
+      "https://github.com/MuhammadMannan/css_480_website/blob/main/my-app/src/assets/image4.png?raw=true",
     heading: "Legends are Born",
     text: "In the heat of battle, when the fate of kingdoms hangs in the balance, true heroes are born. Whether you're facing off against a fearsome dragon, an ancient evil, or a horde of merciless foes, D&D offers an unparalleled opportunity to showcase your valor, strategic prowess, and unwavering determination as you carve your legend into the annals of history.",
   },
   {
-    image: "https://avatars.githubusercontent.com/u/25876150?v=4",
+    image:
+      "https://github.com/MuhammadMannan/css_480_website/blob/main/my-app/src/assets/image5.png?raw=true",
     heading: "Worlds Beyond Worlds",
     text: "From the classic realms of high fantasy to the outer reaches of science fiction and beyond, the multiverse of D&D offers an endless array of worlds to explore. Each setting is a unique tapestry, woven with its own cultures, histories, and mysteries, inviting you to immerse yourself in rich narratives and confront challenges unlike any you've faced before.",
+  },
+  {
+    image:
+      "https://github.com/MuhammadMannan/css_480_website/blob/main/my-app/src/assets/image6.png?raw=true",
+    heading: "Embark on a Timeless Journey",
+    text: "Dungeons & Dragons is a testament to the enduring power of storytelling and the human spirit's desire for adventure. Since its inception, this iconic game has captivated generations of players, transcending the boundaries of time and technology. With every roll of the dice, you become part of a rich legacy that continues to inspire and enchant.",
+  },
+  {
+    image:
+      "https://github.com/MuhammadMannan/css_480_website/blob/main/my-app/src/assets/image7.png?raw=true",
+    heading: "Craft Your Epic Tale",
+    text: "Behind every legendary D&D adventure is a meticulously crafted world, brimming with intricate details and narratives waiting to be unveiled. From the initial spark of inspiration to the intricate web of plot threads and character arcs, the game's creators pour their passion and creativity into ensuring each journey is a truly immersive and unforgettable experience.",
+  },
+  {
+    image:
+      "https://github.com/MuhammadMannan/css_480_website/blob/main/my-app/src/assets/image8.png?raw=true",
+    heading: "Where Heroes are Forged",
+    text: "In the world of D&D, heroes come in many forms – from the stalwart warrior and cunning rogue to the enigmatic sorcerer and noble paladin. Each character class and race offers a unique set of abilities and traits, allowing you to embody a persona that resonates with your playstyle and aspirations, forging an iconic hero destined for greatness.",
   },
 ];
 
@@ -76,8 +98,15 @@ export default function InterestingTopicPage() {
       <div className="pt-20 w-1/2">
         <div className="pb-10">
           <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-            Delving into the Immersive World of D&D
+            Delving into the Immersive World of...
           </h1>
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/en/thumb/8/8e/Dungeons_%26_Dragons_5th_Edition_logo.svg/2880px-Dungeons_%26_Dragons_5th_Edition_logo.svg.png"
+            alt="mannan"
+            width={1000}
+            height={1000}
+            className="rounded-sm, pt-10"
+          />
           <p className="leading-7 [&:not(:first-child)]:mt-6">
             The realm of Dungeons & Dragons is a vast and captivating one, where
             imagination knows no bounds. This iconic tabletop roleplaying game
@@ -93,38 +122,13 @@ export default function InterestingTopicPage() {
           </p>
 
           <Image
-            src="https://upload.wikimedia.org/wikipedia/en/thumb/8/8e/Dungeons_%26_Dragons_5th_Edition_logo.svg/2880px-Dungeons_%26_Dragons_5th_Edition_logo.svg.png"
+            src="https://cdn.vox-cdn.com/thumbor/lloVcNf2ZNmsgptLA0LlqlIi04k=/0x0:7952x5304/3570x2008/filters:focal(3340x2016:4612x3288):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/67811989/178_Mars_9780593157435_lay_art_r1.0.jpg"
             alt="mannan"
             width={1000}
             height={1000}
             className="rounded-sm, pt-10"
           />
-          <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
-            The King&apos;s Plan
-          </h2>
-          <p className="leading-7 [&:not(:first-child)]:mt-6">
-            One of the most alluring aspects of D&D is its versatility. The
-            game&apos;s rulebooks serve as a foundation, providing a framework
-            for character creation, combat encounters, and various game
-            mechanics. However, the true magic lies in the boundless creativity
-            of the players and the Dungeon Master (DM), who orchestrates the
-            adventure. From sprawling medieval fantasy realms to futuristic
-            sci-fi settings, the possibilities are endless, allowing players to
-            craft their own unique stories and worlds. Beyond the thrilling
-            gameplay, D&D fosters a strong sense of community. Gaming groups
-            often form lasting friendships, bonding over shared experiences and
-            inside jokes that only those who have faced perilous challenges
-            together can truly appreciate. The game encourages cooperation,
-            problem-solving, and strategic thinking, making it a captivating and
-            enriching experience for players of all ages and backgrounds. In a
-            world where digital entertainment dominates, D&D offers a refreshing
-            return to the simplicity of pencils, paper, and the boundless
-            expanse of the imagination. Whether you&apos;re a seasoned
-            adventurer or a newcomer to the world of tabletop roleplaying, the
-            allure of D&D is undeniable – a testament to the enduring power of
-            storytelling and the human spirit&apos;s desire for escapism and
-            camaraderie.
-          </p>
+
           <h3 className="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight">
             Explore the Realms of Adventure
           </h3>
@@ -163,6 +167,32 @@ export default function InterestingTopicPage() {
               <CarouselNext />
             </Carousel>
           </div>
+          <h2 className="mt-5 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+            The King&apos;s Plan
+          </h2>
+          <p className="leading-7 [&:not(:first-child)]:mt-6">
+            One of the most alluring aspects of D&D is its versatility. The
+            game&apos;s rulebooks serve as a foundation, providing a framework
+            for character creation, combat encounters, and various game
+            mechanics. However, the true magic lies in the boundless creativity
+            of the players and the Dungeon Master (DM), who orchestrates the
+            adventure. From sprawling medieval fantasy realms to futuristic
+            sci-fi settings, the possibilities are endless, allowing players to
+            craft their own unique stories and worlds. Beyond the thrilling
+            gameplay, D&D fosters a strong sense of community. Gaming groups
+            often form lasting friendships, bonding over shared experiences and
+            inside jokes that only those who have faced perilous challenges
+            together can truly appreciate. The game encourages cooperation,
+            problem-solving, and strategic thinking, making it a captivating and
+            enriching experience for players of all ages and backgrounds. In a
+            world where digital entertainment dominates, D&D offers a refreshing
+            return to the simplicity of pencils, paper, and the boundless
+            expanse of the imagination. Whether you&apos;re a seasoned
+            adventurer or a newcomer to the world of tabletop roleplaying, the
+            allure of D&D is undeniable – a testament to the enduring power of
+            storytelling and the human spirit&apos;s desire for escapism and
+            camaraderie.
+          </p>
         </div>
       </div>
     </div>
